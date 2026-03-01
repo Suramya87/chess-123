5f9895c1f2be7371665d57f5228c72e954d5c01a
Suramya <146380955+Suramya87@users.noreply.github.com>
2026-02-28 18:48:50 -0800

    ACTUCLLY PUSHING MY CHANGES

--- classes/Chess.cpp
+++ classes/Chess.cpp
@@ -46,11 +46,13 @@ void Chess::setUpBoard()
     _grid->initializeChessSquares(pieceSize, "boardsquare.png");
     FENtoBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     startGame();
+    
+    std::vector<BitMove> initialMoves = generateAllMoves(0);  
+    size_t moveCount = initialMoves.size();  
 }
 
 void Chess::FENtoBoard(const std::string& fen)
 {
-    // map FEN characters to their corresponding piece types
     auto charToPiece = [](char c) -> ChessPiece {
         switch (tolower(c)) {
             case 'p': return Pawn;
@@ -64,10 +66,10 @@ void Chess::FENtoBoard(const std::string& fen)
     };
 
     int col = 0;
-    int rank = 7; // FEN starts at rank 8 (y=7), works down to rank 1 (y=0)
+    int rank = 7;
 
     for (char c : fen) {
-        if (c == ' ') break;  // stop at the end of board position field
+        if (c == ' ') break;
 
         if (c == '/') {
             col = 0;
@@ -88,6 +90,361 @@ void Chess::FENtoBoard(const std::string& fen)
     }
 }
 
+int Chess::squareToIndex(int x, int y) const
+{
+    return y * 8 + x;
+}
+
+void Chess::indexToSquare(int index, int& x, int& y) const
+{
+    x = index % 8;
+    y = index / 8;
+}
+
+bool Chess::isValidSquare(int x, int y) const
+{
+    return x >= 0 && x < 8 && y >= 0 && y < 8;
+}
+
+bool Chess::isOccupied(int x, int y) const
+{
+    if (!isValidSquare(x, y)) return false;
+    BitHolder* holder = _grid->getSquare(x, y);
+    return holder && holder->bit() != nullptr;
+}
+
+bool Chess::isEnemyPiece(int x, int y, int playerNumber) const
+{
+    if (!isValidSquare(x, y)) return false;
+    Bit* bit = _grid->getSquare(x, y)->bit();
+    if (!bit) return false;
+    int pieceOwner = (bit->gameTag() & 128) ? 1 : 0;
+    return pieceOwner != playerNumber;
+}
+
+bool Chess::isFriendlyPiece(int x, int y, int playerNumber) const
+{
+    if (!isValidSquare(x, y)) return false;
+    Bit* bit = _grid->getSquare(x, y)->bit();
+    if (!bit) return false;
+    int pieceOwner = (bit->gameTag() & 128) ? 1 : 0;
+    return pieceOwner == playerNumber;
+}
+
+int Chess::getPieceType(Bit* bit) const
+{
+    if (!bit) return 0;
+    return bit->gameTag() & 0x7F;
+}
+
+int Chess::getPieceTypeAt(int x, int y) const
+{
+    if (!isValidSquare(x, y)) return 0;
+    Bit* bit = _grid->getSquare(x, y)->bit();
+    return getPieceType(bit);
+}
+
+uint64_t Chess::getOccupancyBitboard() const
+{
+    uint64_t occupancy = 0ULL;
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            if (isOccupied(x, y)) {
+                occupancy |= (1ULL << squareToIndex(x, y));
+            }
+        }
+    }
+    return occupancy;
+}
+
+uint64_t Chess::getFriendlyBitboard(int playerNumber) const
+{
+    uint64_t friendly = 0ULL;
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            if (isFriendlyPiece(x, y, playerNumber)) {
+                friendly |= (1ULL << squareToIndex(x, y));
+            }
+        }
+    }
+    return friendly;
+}
+
+uint64_t Chess::getEnemyBitboard(int playerNumber) const
+{
+    uint64_t enemy = 0ULL;
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            if (isEnemyPiece(x, y, playerNumber)) {
+                enemy |= (1ULL << squareToIndex(x, y));
+            }
+        }
+    }
+    return enemy;
+}
+
+uint64_t Chess::getBitboardForPieceType(int pieceType, int playerNumber) const
+{
+    uint64_t bb = 0ULL;
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            if (getPieceTypeAt(x, y) == pieceType && isFriendlyPiece(x, y, playerNumber)) {
+                bb |= (1ULL << squareToIndex(x, y));
+            }
+        }
+    }
+    return bb;
+}
+
+std::vector<BitMove> Chess::generatePawnMoves(int playerNumber)
+{
+    std::vector<BitMove> moves;
+    uint64_t pawns = getBitboardForPieceType(Pawn, playerNumber);
+    
+    int direction = (playerNumber == 0) ? 1 : -1;
+    int startRank = (playerNumber == 0) ? 1 : 6;
+    
+    for (int i = 0; i < 64; i++) {
+        if (!(pawns & (1ULL << i))) continue;
+        
+        int x, y;
+        indexToSquare(i, x, y);
+        
+        int newY = y + direction;
+        if (isValidSquare(x, newY) && !isOccupied(x, newY)) {
+            moves.emplace_back(i, squareToIndex(x, newY), Pawn);
+            
+            if (y == startRank) {
+                int doubleY = y + 2 * direction;
+                if (!isOccupied(x, doubleY)) {
+                    moves.emplace_back(i, squareToIndex(x, doubleY), Pawn);
+                }
+            }
+        }
+        
+        int captureX[] = {x - 1, x + 1};
+        for (int cx : captureX) {
+            if (isValidSquare(cx, newY) && isEnemyPiece(cx, newY, playerNumber)) {
+                moves.emplace_back(i, squareToIndex(cx, newY), Pawn);
+            }
+        }
+    }
+    
+    return moves;
+}
+
+std::vector<BitMove> Chess::generateKnightMoves(int playerNumber)
+{
+    std::vector<BitMove> moves;
+    uint64_t knights = getBitboardForPieceType(Knight, playerNumber);
+    uint64_t friendly = getFriendlyBitboard(playerNumber);
+    
+    int knightOffsets[8][2] = {
+        {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+        {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+    };
+    
+    for (int i = 0; i < 64; i++) {
+        if (!(knights & (1ULL << i))) continue;
+        
+        int x, y;
+        indexToSquare(i, x, y);
+        
+        for (const auto& offset : knightOffsets) {
+            int newX = x + offset[0];
+            int newY = y + offset[1];
+            
+            if (isValidSquare(newX, newY)) {
+                int targetSquare = squareToIndex(newX, newY);
+                if (!(friendly & (1ULL << targetSquare))) {
+                    moves.emplace_back(i, targetSquare, Knight);
+                }
+            }
+        }
+    }
+    
+    return moves;
+}
+
+std::vector<BitMove> Chess::generateBishopMoves(int playerNumber)
+{
+    std::vector<BitMove> moves;
+    uint64_t bishops = getBitboardForPieceType(Bishop, playerNumber);
+    uint64_t friendly = getFriendlyBitboard(playerNumber);
+    
+    int directions[4][2] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+    
+    for (int i = 0; i < 64; i++) {
+        if (!(bishops & (1ULL << i))) continue;
+        
+        int x, y;
+        indexToSquare(i, x, y);
+        
+        for (const auto& dir : directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            
+            while (isValidSquare(newX, newY)) {
+                int targetSquare = squareToIndex(newX, newY);
+                
+                if (friendly & (1ULL << targetSquare)) break;
+                
+                moves.emplace_back(i, targetSquare, Bishop);
+                
+                if (isOccupied(newX, newY)) break;
+                
+                newX += dir[0];
+                newY += dir[1];
+            }
+        }
+    }
+    
+    return moves;
+}
+
+std::vector<BitMove> Chess::generateRookMoves(int playerNumber)
+{
+    std::vector<BitMove> moves;
+    uint64_t rooks = getBitboardForPieceType(Rook, playerNumber);
+    uint64_t friendly = getFriendlyBitboard(playerNumber);
+    
+    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    for (int i = 0; i < 64; i++) {
+        if (!(rooks & (1ULL << i))) continue;
+        
+        int x, y;
+        indexToSquare(i, x, y);
+        
+        for (const auto& dir : directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            
+            while (isValidSquare(newX, newY)) {
+                int targetSquare = squareToIndex(newX, newY);
+                
+                if (friendly & (1ULL << targetSquare)) break;
+                
+                moves.emplace_back(i, targetSquare, Rook);
+                
+                if (isOccupied(newX, newY)) break;
+                
+                newX += dir[0];
+                newY += dir[1];
+            }
+        }
+    }
+    
+    return moves;
+}
+
+std::vector<BitMove> Chess::generateQueenMoves(int playerNumber)
+{
+    std::vector<BitMove> moves;
+    uint64_t queens = getBitboardForPieceType(Queen, playerNumber);
+    uint64_t friendly = getFriendlyBitboard(playerNumber);
+    
+    int directions[8][2] = {
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1},
+        {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+    };
+    
+    for (int i = 0; i < 64; i++) {
+        if (!(queens & (1ULL << i))) continue;
+        
+        int x, y;
+        indexToSquare(i, x, y);
+        
+        for (const auto& dir : directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            
+            while (isValidSquare(newX, newY)) {
+                int targetSquare = squareToIndex(newX, newY);
+                
+                if (friendly & (1ULL << targetSquare)) break;
+                
+                moves.emplace_back(i, targetSquare, Queen);
+                
+                if (isOccupied(newX, newY)) break;
+                
+                newX += dir[0];
+                newY += dir[1];
+            }
+        }
+    }
+    
+    return moves;
+}
+
+std::vector<BitMove> Chess::generateKingMoves(int playerNumber)
+{
+    std::vector<BitMove> moves;
+    uint64_t kings = getBitboardForPieceType(King, playerNumber);
+    uint64_t friendly = getFriendlyBitboard(playerNumber);
+    
+    int directions[8][2] = {
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1},
+        {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+    };
+    
+    for (int i = 0; i < 64; i++) {
+        if (!(kings & (1ULL << i))) continue;
+        
+        int x, y;
+        indexToSquare(i, x, y);
+        
+        for (const auto& dir : directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            
+            if (isValidSquare(newX, newY)) {
+                int targetSquare = squareToIndex(newX, newY);
+                
+                if (!(friendly & (1ULL << targetSquare))) {
+                    moves.emplace_back(i, targetSquare, King);
+                }
+            }
+        }
+    }
+    
+    return moves;
+}
+
+std::vector<BitMove> Chess::generateAllMoves(int playerNumber)
+{
+    std::vector<BitMove> allMoves;
+    
+    auto pawnMoves = generatePawnMoves(playerNumber);
+    auto knightMoves = generateKnightMoves(playerNumber);
+    auto bishopMoves = generateBishopMoves(playerNumber);
+    auto rookMoves = generateRookMoves(playerNumber);
+    auto queenMoves = generateQueenMoves(playerNumber);
+    auto kingMoves = generateKingMoves(playerNumber);
+    
+    allMoves.insert(allMoves.end(), pawnMoves.begin(), pawnMoves.end());
+    allMoves.insert(allMoves.end(), knightMoves.begin(), knightMoves.end());
+    allMoves.insert(allMoves.end(), bishopMoves.begin(), bishopMoves.end());
+    allMoves.insert(allMoves.end(), rookMoves.begin(), rookMoves.end());
+    allMoves.insert(allMoves.end(), queenMoves.begin(), queenMoves.end());
+    allMoves.insert(allMoves.end(), kingMoves.begin(), kingMoves.end());
+    
+    return allMoves;
+}
+
+bool Chess::isLegalMove(const BitMove& move, int playerNumber)
+{
+    std::vector<BitMove> legalMoves = generateAllMoves(playerNumber);
+    
+    for (const auto& legal : legalMoves) {
+        if (legal == move) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
 bool Chess::actionForEmptyHolder(BitHolder &holder)
 {
     return false;
@@ -95,14 +452,36 @@ bool Chess::actionForEmptyHolder(BitHolder &holder)
 
 bool Chess::canBitMoveFrom(Bit &bit, BitHolder &src)
 {
-    int currentPlayer = getCurrentPlayer()->playerNumber() * 128;
-    int pieceColor = bit.gameTag() & 128;
+    int currentPlayer = getCurrentPlayer()->playerNumber();
+    int pieceColor = (bit.gameTag() & 128) ? 1 : 0;
     return pieceColor == currentPlayer;
 }
 
 bool Chess::canBitMoveFromTo(Bit &bit, BitHolder &src, BitHolder &dst)
 {
-    return true;
+    int srcX = 0, srcY = 0;
+    int dstX = 0, dstY = 0;
+    
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            if (_grid->getSquare(x, y) == &src) {
+                srcX = x;
+                srcY = y;
+            }
+            if (_grid->getSquare(x, y) == &dst) {
+                dstX = x;
+                dstY = y;
+            }
+        }
+    }
+    
+    int currentPlayer = getCurrentPlayer()->playerNumber();
+    int fromSquare = squareToIndex(srcX, srcY);
+    int toSquare = squareToIndex(dstX, dstY);
+    int pieceType = getPieceType(&bit);
+    
+    BitMove move(fromSquare, toSquare, pieceType);
+    return isLegalMove(move, currentPlayer);
 }
 
 void Chess::stopGame()
@@ -156,4 +535,9 @@ void Chess::setStateString(const std::string &s)
             square->setBit(nullptr);
         }
     });
+}
+
+Grid* Chess::getGrid()
+{
+    return _grid;
 }
--- classes/Chess.h
+++ classes/Chess.h
@@ -2,6 +2,8 @@
 
 #include "Game.h"
 #include "Grid.h"
+#include <vector>
+#include <cstdint>
 
 constexpr int pieceSize = 80;
 
@@ -14,6 +16,23 @@ enum ChessPiece
     Rook,
     Queen,
     King
+};
+
+struct BitMove {
+    uint8_t from;
+    uint8_t to;
+    uint8_t piece;
+    
+    BitMove(int from, int to, int piece)
+        : from(from), to(to), piece(piece) { }
+        
+    BitMove() : from(0), to(0), piece(NoPiece) { }
+    
+    bool operator==(const BitMove& other) const {
+        return from == other.from && 
+               to == other.to && 
+               piece == other.piece;
+    }
 };
 
 class Chess : public Game
@@ -37,13 +56,41 @@ public:
     std::string stateString() override;
     void setStateString(const std::string &s) override;
 
-    Grid* getGrid() override { return _grid; }
+    Grid* getGrid() override;
+    
+    // Move generation
+    std::vector<BitMove> generateAllMoves(int playerNumber);
 
 private:
     Bit* PieceForPlayer(const int playerNumber, ChessPiece piece);
     Player* ownerAt(int x, int y) const;
     void FENtoBoard(const std::string& fen);
     char pieceNotation(int x, int y) const;
+    
+    // Bitboard helpers
+    uint64_t getOccupancyBitboard() const;
+    uint64_t getFriendlyBitboard(int playerNumber) const;
+    uint64_t getEnemyBitboard(int playerNumber) const;
+    uint64_t getBitboardForPieceType(int pieceType, int playerNumber) const;
+    
+    // Move generation for each piece type
+    std::vector<BitMove> generatePawnMoves(int playerNumber);
+    std::vector<BitMove> generateKnightMoves(int playerNumber);
+    std::vector<BitMove> generateBishopMoves(int playerNumber);
+    std::vector<BitMove> generateRookMoves(int playerNumber);
+    std::vector<BitMove> generateQueenMoves(int playerNumber);
+    std::vector<BitMove> generateKingMoves(int playerNumber);
+    
+    // Helper functions
+    bool isValidSquare(int x, int y) const;
+    bool isOccupied(int x, int y) const;
+    bool isEnemyPiece(int x, int y, int playerNumber) const;
+    bool isFriendlyPiece(int x, int y, int playerNumber) const;
+    int getPieceType(Bit* bit) const;
+    int getPieceTypeAt(int x, int y) const;
+    int squareToIndex(int x, int y) const;
+    void indexToSquare(int index, int& x, int& y) const;
+    bool isLegalMove(const BitMove& move, int playerNumber);
 
     Grid* _grid;
 };
